0c550f01aa50a7de5fa4e503acfa0f43ad358437
Luis Arce <luis.arce22@gmail.com>
2020-11-26 19:26:17 -0400

    write unit tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,7 +47,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.include FActoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
