80b8aba404b465b4f1e1b9d47f973aac3795819f
Luis Arce <luis.arce22@gmail.com>
2021-07-01 00:20:02 -0400

    fix migration and create routes and controller for projects

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :todos do
     resources :items
   end
+
+  resources :projects
 end
--- db/migrate/20210701040736_create_projects.rb
+++ db/migrate/20210701040736_create_projects.rb
@@ -3,7 +3,6 @@ class CreateProjects < ActiveRecord::Migration[6.0]
     create_table :projects do |t|
       t.string :name
       t.text :description
-      t.timestamps :created_at
       t.boolean :active
 
       t.timestamps
